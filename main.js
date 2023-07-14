fb32c98d7684a19cec3f3b4fa0f906a5936e2080
ColluraV <valerio.18.02.89@hotmail.it>
2023-07-14 16:03:08 +0200

    completo

--- main.js
+++ main.js
@@ -32,3 +32,41 @@ let ListaTeam= [
         img: "barbara-ramos-graphic-designer.jpg",
     }
 ]
+
+
+for (let i = 0; i<ListaTeam.length; i++){
+    const membro = ListaTeam[i];
+    
+    console.log("Elemento team n° ", (i+1)," ",membro);
+
+
+
+    cardContainer.innerHTML += (
+    `<div class="card col-3" style="width: 18rem;">
+        <img src="/img/${membro["img"]}" class="card-img-top" alt="${membro["name"]}">
+            <div class="card-body">
+                <h5 class="card-title">${membro["name"]}</h5>
+                <p class="card-text">${membro["role"]}</p>
+      
+              </div>
+     </div>`
+    
+    )
+
+}
+
+
+
+
+
+
+//card div vuoto Bootstrap
+
+/*<div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      
+                    </div>
+                </div> */
